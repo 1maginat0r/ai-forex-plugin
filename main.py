91b010a7be3d4385f6354dc7909ee7fae0b3aa9c
1maginat0r <imaginai90@proton.me>
2021-05-30 14:33:03 +0000

    v3.7.2

--- main.py
+++ main.py
@@ -26,3 +26,38 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
   # yaml_data = yaml.safe_load(yaml_file)
    
 token = os.getenv('OANDA_TOKEN')
+accounts = [os.getenv('ACCOUNT1'), os.getenv('ACCOUNT2')]
+active_account = os.getenv('ACTIVE_ACCOUNT')
+username = os.getenv('OANDA_USERNAME')
+
+# Set values from environment variables (removing in production)
+#yaml_data['oanda']['token'] = os.getenv('OANDA_TOKEN')
+#yaml_data['oanda']['accounts'] = [os.getenv('ACCOUNT1'), os.getenv('ACCOUNT2')]
+#yaml_data['oanda']['active_account'] = os.getenv('ACTIVE_ACCOUNT')
+#yaml_data['oanda']['username'] = os.getenv('OANDA_USERNAME')
+
+# Save modified YAML file (removing in production)
+#with open('openapi.yaml', 'w') as yaml_file:
+ # yaml.dump(yaml_data, yaml_file)
+
+# Setup Flask app
+app = Flask(__name__)
+
+# Enable CORS and configure CORS Headers
+cors = CORS(app, resources={
+    r"/*": {
+        "origins": "https://chat.openai.com",  # Update this to the origin you want to allow
+        "allow_headers": [
+            "Content-Type",
+            "Authorization",
+            "Access-Control-Allow-Credentials",
+            "Access-Control-Allow-Headers",
+            "Access-Control-Allow-Methods",
+            "Access-Control-Allow-Origin",
+            "Baggage",
+            "sentry-trace",
+            "openai-conversation-id",
+            "openai-ephemeral-user-id"  
+        ]
+    }
+})
