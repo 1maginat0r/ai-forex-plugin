e7c2f75b5516e1506ac08d2916109b0670808b48
1maginat0r <imaginai90@proton.me>
2021-06-07 09:59:25 +0000

    add php

--- main.py
+++ main.py
@@ -161,3 +161,34 @@ def get_prices():
     plugin = OpenAIPlugin(oanda_api_key, openai_api_key)
 
     candles = plugin.get_oanda_candles(instrument, from_time, granularity, price)
+
+    if not candles:
+        return jsonify({'message': "Failed to fetch candles."}), 500
+
+    return jsonify({'candles': candles})
+
+
+from datetime import datetime, timedelta
+
+@app.route('/api/analyze', methods=['POST'])
+def analyze():
+    data = request.get_json()
+
+    instrument = data.get('instrument', 'EUR_USD')
+    granularity = data.get('granularity', 'H1')
+    price = data.get('price', 'M')
+
+    # If 'from_time' is not provided in the request, calculate it as 2 days before the current time
+    if 'from_time' not in data:
+        from_time = (datetime.utcnow() - timedelta(days=2)).isoformat() + 'Z'
+    else:
+        from_time = data.get('from_time')
+
+    oanda_api_key = os.getenv("OANDA_API_KEY")
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+
+    plugin = OpenAIPlugin(oanda_api_key, openai_api_key)
+
+    sentiment = plugin.analyze_market(instrument, from_time, granularity, price)
+
+    print(f"Market sentiment: {sentiment}")
